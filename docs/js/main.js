a85c373d0193f6101303d41a10f79215560a1047
andrea zuluaga <karenzu321@gmail.com>
2023-01-15 00:16:01 -0500

    avances en mi portafolio

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,12 @@
-console.log("andrea zuluaga monroy")
+'use strict';
+const verdadero = true
+const falso = false 
+const vObj = new Boolean(true)
+const fObj = new Boolean(false)
+const resultado = 'javascript' === 'java';
+
+// console.log(falso);
+// console.log(verdadero);
+// console.log(resultado);
+
+
